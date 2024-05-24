0d665fb81278f412da7f3fffc6060ac267f03415
Thevesh <thevesh.theva@gmail.com>
2024-05-24 17:21:31 +0800

    hhincome: add scatterplot

--- 2024-05-24_hhincome/script.py
+++ 2024-05-24_hhincome/script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.ticker import StrMethodFormatter
 import seaborn as sb
 
 
@@ -115,6 +116,67 @@ def bar_state():
     plt.close()
 
 
+def scatter_state():
+    TARGET = ['M40','T20','T10','T5','T1','T0.5']
+    TARGET_PLOT = ['M40','T20','T10','T1']
+    PLOT_COLOURS = ['black','#8d99ae','blue','red']
+    COLOURS = dict(zip(TARGET_PLOT,PLOT_COLOURS))
+
+    df = pd.read_parquet('https://storage.dosm.gov.my/hies/hies_state_percentile.parquet')
+    df = df[df.date == df.date.max()].drop('date',axis=1)
+    df = pd.concat([
+        df[(df.variable == 'minimum') & (df.percentile.isin([41,81,91,96,100]))],
+        df[(df.variable == 'median') & (df.percentile.isin([100]))]
+    ],axis=0,ignore_index=True).drop('variable',axis=1)
+    df = df.sort_values(by=['state','percentile'])
+    df.percentile = TARGET*16
+    df = df.pivot(index='state',columns='percentile',values='income')[TARGET_PLOT].sort_values(by='M40')
+
+    plt.rcParams.update({'font.size': 10,
+                        'font.family': 'sans-serif',
+                        'grid.linestyle': 'dashed'})
+    plt.rcParams["figure.figsize"] = [6.5,6.5]
+    plt.rcParams["figure.autolayout"] = True
+    fig, ax = plt.subplots()
+
+    v = 'income'
+    for t in TARGET_PLOT: 
+        ax.scatter(df[t], df.index, color=COLOURS[t], marker='o', s=50, label=t)
+
+    # plot-wide adjustments
+    ax.set_title(f'Household Income by State and Percentile\n\n',linespacing=2,fontsize=11)
+    for b in ['top','right']: ax.spines[b].set_visible(False)
+    for b in ['bottom','left']: ax.spines[b].set_color('#cacaca')
+    ax.set_axisbelow(True)
+    ax.tick_params(axis=u'both', which=u'both',length=0)
+
+    # y-axis adjustments
+    ax.set_ylabel('')
+    ax.yaxis.grid(True)
+
+    # x-axis adjustments
+    ax.set_xlabel('')
+    ax.set_xlim(0)
+    ax.xaxis.grid(True)
+    ax.get_xaxis().set_visible(True)
+    plt.xticks(rotation=0)
+    ax.xaxis.set_major_formatter(StrMethodFormatter('{x:,.0f}'))
+
+    # legend
+    handles, labels = plt.gca().get_legend_handles_labels()
+    order = [x for x in range(len(TARGET_PLOT))]
+    ax.legend([handles[idx] for idx in order],[labels[idx] for idx in order], labelspacing=1, 
+            loc='upper center', framealpha=0.7, ncols=5, bbox_to_anchor=(0.5, 1.1))
+
+    # ALT text
+    for s in df.index:
+        print(f"{s}: {df.loc[s,'M40']/1000:,.1f}k, {df.loc[s,'T20']/1000:,.1f}k, {df.loc[s,'T10']/1000:,.1f}k, {df.loc[s,'T1']/1000:,.1f}k")
+
+    plt.savefig('scatter_state.png',dpi=400)
+    plt.close()
+
+
 if __name__ == '__main__':
     bar_malaysia()
     bar_state()
+    scatter_state()
